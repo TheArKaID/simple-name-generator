d426c1bd89c1490f3cd55d7c280bf107c75b0309
Arifia Kasastra R <arka.progammer@gmail.com>
2024-07-03 15:45:33 +0700

    Add generateRandomString method

--- src/RandomNameGenerator.php
+++ src/RandomNameGenerator.php
@@ -9,4 +9,24 @@ class RandomNameGenerator
         $this->vowels = 'aeiou';
         $this->consonants = 'bcdfghjklmnpqrstvwxyz';
     }
+
+    private function generateRandomString(int $length): string
+    {
+        if ($length < 3) {
+            $length = 3;
+        }
+
+        $pattern = 'CVCVC';
+        $randomString = '';
+
+        foreach (str_split($pattern) as $char) {
+            if ($char === 'C') {
+                $randomString .= $this->consonants[rand(0, strlen($this->consonants) - 1)];
+            } else {
+                $randomString .= $this->vowels[rand(0, strlen($this->vowels) - 1)];
+            }
+        }
+
+        return ucfirst($randomString);
+    }
 }
